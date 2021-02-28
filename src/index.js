33751afecf90277d96f9f9bb4f276a29e04b64c1
scott95100 <scott95100@gmail.com>
2021-02-28 13:14:54 -0700

    Build out complete to declairing a winner, next to impliment time travel

--- src/index.js
+++ src/index.js
@@ -19,8 +19,12 @@ function Square(props) {
         xIsNext: true,
       };
     }
+
     handleClick(i) {
       const squares = this.state.squares.slice();
+      if ( calculateWinner(squares) ||squares[i]) {
+        return;
+      }
       squares[i] = this.state.xIsNext ? 'X' : 'O';
       this.setState({
         squares: squares,
@@ -38,7 +42,13 @@ function Square(props) {
     }
   
     render() {
-      const status = 'Next player: X';
+      const winner = calculateWinner(this.state.squares);
+      let status;
+      if (winner) {
+        status = 'Winner: ' + winner;
+      } else {
+        status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+      }
   
       return (
         <div>
@@ -78,6 +88,26 @@ function Square(props) {
       );
     }
   }
+
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
   
   // ========================================
   
@@ -85,3 +115,4 @@ function Square(props) {
     <Game />,
     document.getElementById('root')
   );
+
